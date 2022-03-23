a050cb8e1c4e4d6daf3388c4916144146f8b9450
Nathalie Blasberg <bettina.blasberg@yahoo.de>
2022-03-23 22:57:05 +0100

    add router link

--- src/app/poke-dex/poke-dex.component.ts
+++ src/app/poke-dex/poke-dex.component.ts
@@ -29,7 +29,7 @@ export class PokeDexComponent implements OnInit {
     this.pokedexList$ = pokeList.results;
     this.numberPages = Math.ceil(pokeList.count / 20);
     this.currentPage = Math.floor(this.offset / this.limit);
-    this.pagesList = Array(Math.ceil(pokeList.count / 20)).fill(0).map((x,i)=>i);
+    this.pagesList = Array(Math.ceil(pokeList.count / 20)).fill(0).map((_,i)=>i);
   }
 
   public onButtonClick(newOffset: number): void {
