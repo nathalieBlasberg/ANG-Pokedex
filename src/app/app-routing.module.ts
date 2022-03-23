42b31f4b345e0846e8271f8a053394f47b043c82
Nathalie Blasberg <bettina.blasberg@yahoo.de>
2022-03-23 22:54:55 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeDexComponent } from './poke-dex/poke-dex.component';
+import { PokemonEntryComponent } from './pokemon-entry/pokemon-entry.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'},
+  { path: 'home', component: PokeDexComponent },
+  { path: 'pokemon/:name', component: PokemonEntryComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
