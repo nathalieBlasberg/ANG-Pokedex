565c68161fc983ec3d06b0c89bd8f1f5adb1738f
Nathalie Blasberg <bettina.blasberg@yahoo.de>
2022-03-24 18:12:01 +0100

    add sprites to pokedex

--- src/app/poke-dex/poke-dex.component.ts
+++ src/app/poke-dex/poke-dex.component.ts
@@ -8,13 +8,15 @@ import { PokedexList, Pokedex } from '../pokedex.model';
   styleUrls: ['./poke-dex.component.scss']
 })
 export class PokeDexComponent implements OnInit {
+  private spriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+  
   public pokedexList$: PokedexList[] = [];
-
   public numberPages: number = 0;
   public offset: number = 0;
   public limit: number = 20;
   public currentPage: number = 1;
   public pagesList: number[] = [];
+  
 
   constructor(
     private pokeData: PokeDataService
@@ -29,7 +31,14 @@ export class PokeDexComponent implements OnInit {
     this.pokedexList$ = pokeList.results;
     this.numberPages = Math.ceil(pokeList.count / 20);
     this.currentPage = Math.floor(this.offset / this.limit);
-    this.pagesList = Array(Math.ceil(pokeList.count / 20)).fill(0).map((_,i)=>i);
+    this.pagesList = Array(Math.ceil(pokeList.count / 20)).fill(0).map((_, i) => i);
+    this.pokedexList$.forEach(pokeEntry => {
+      const pokeId = pokeEntry.url.match(/^https:\/\/pokeapi.co\/api\/v2\/pokemon\/(\d+)\/$/);
+      if (pokeId === null) {
+        return;
+      }
+      pokeEntry.url = `${this.spriteUrl}${pokeId[1]}.png`;
+    });
   }
 
   public onButtonClick(newOffset: number): void {
