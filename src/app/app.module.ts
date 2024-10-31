72738154fe2adc1013270bd615ca99b9796aee9f
Bettina Blasberg <bettina.blasberg@yahoo.de>
2024-11-01 00:28:06 +0100

    add components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PokeDexComponent } from './poke-dex/poke-dex.component';
-import { HeaderComponent } from './header/header.component';
-import { PaginationComponent } from './pagination/pagination.component';
-import { PokemonEntryComponent } from './pokemon-entry/pokemon-entry.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PokeDexComponent } from "./poke-dex/poke-dex.component";
+import { HeaderComponent } from "./header/header.component";
+import { PaginationComponent } from "./pagination/pagination.component";
+import { PokemonEntryComponent } from "./pokemon-entry/pokemon-entry.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { PokeDexEntryComponent } from "./poke-dex-entry/poke-dex-entry.component";
+import { OrderPipe } from "src/pipes/order.pipe";
 
 @NgModule({
   declarations: [
@@ -16,14 +18,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HeaderComponent,
     PaginationComponent,
     PokemonEntryComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PokeDexEntryComponent,
+    OrderPipe,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
