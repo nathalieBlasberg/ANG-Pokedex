95ddbf25938383f50aaf8d7c324d1ab8bdde2e77
Bettina Blasberg <bettina.blasberg@yahoo.de>
2024-11-01 00:59:31 +0100

    refactoring

--- src/app/poke-dex/poke-dex.component.ts
+++ src/app/poke-dex/poke-dex.component.ts
@@ -1,58 +1,47 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { PokeDataService } from "../../services/poke-data.service";
-import {
-  NamedAPIResource,
-  NamedAPIResourceList,
-} from "../../models/pokedex.model";
-import { Subject, take, takeUntil } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { PokeDataService } from '../poke-data.service';
+import { PokedexList, Pokedex } from '../pokedex.model';
 
 @Component({
-  selector: "app-poke-dex",
-  templateUrl: "./poke-dex.component.html",
-  styleUrls: ["./poke-dex.component.scss"],
+  selector: 'app-poke-dex',
+  templateUrl: './poke-dex.component.html',
+  styleUrls: ['./poke-dex.component.scss']
 })
 export class PokeDexComponent implements OnInit {
-  public pokedexList$: NamedAPIResource[] = [];
+  private spriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+  
+  public pokedexList$: PokedexList[] = [];
   public numberPages: number = 0;
   public offset: number = 0;
   public limit: number = 20;
   public currentPage: number = 1;
   public pagesList: number[] = [];
+  
 
-  constructor(private pokeData: PokeDataService) {}
+  constructor(
+    private pokeData: PokeDataService
+  ) {}
 
   public ngOnInit(): void {
-    this.fetchPokemonList();
+    this.pokeData.fetchPokemonData(`offset=${this.offset}&limit=${this.limit}`)
+    .subscribe(response => this.onFetchedPokemonData(response));
   }
 
-  public fetchPokemonList(): void {
-    this.pokeData
-      .fetchPokemonData(`offset=${this.offset}&limit=${this.limit}`)
-      .pipe(take(1))
-      .subscribe({
-        next: (next: NamedAPIResourceList) => this.onFetchedPokemonData(next),
-      });
-  }
-
-  public onFetchedPokemonData(pokeList: NamedAPIResourceList): void {
+  public onFetchedPokemonData(pokeList: Pokedex): void {
     this.pokedexList$ = pokeList.results;
     this.numberPages = Math.ceil(pokeList.count / 20);
     this.currentPage = Math.floor(this.offset / this.limit);
-    /* this.pagesList = Array(Math.ceil(pokeList.count / 20))
-      .fill(0)
-      .map((_, i) => i);
-    this.pokedexList$.forEach((pokeEntry) => {
-      const pokeId = pokeEntry.url.match(
-        /^https:\/\/pokeapi.co\/api\/v2\/pokemon\/(\d+)\/$/
-      );
+    this.pagesList = Array(Math.ceil(pokeList.count / 20)).fill(0).map((_, i) => i);
+    this.pokedexList$.forEach(pokeEntry => {
+      const pokeId = pokeEntry.url.match(/^https:\/\/pokeapi.co\/api\/v2\/pokemon\/(\d+)\/$/);
       if (pokeId === null) {
         return;
       }
       pokeEntry.url = `${this.spriteUrl}${pokeId[1]}.png`;
-    }); */
+    });
   }
 
-  /* public onButtonClick(newOffset: number): void {
+  public onButtonClick(newOffset: number): void {
     this.offset = newOffset;
 
     if (this.offset < 0) {
@@ -61,8 +50,7 @@ export class PokeDexComponent implements OnInit {
 
     let url = `offset=${this.offset}&limit=${this.limit}`;
 
-    this.pokeData
-      .fetchPokemonData(url)
-      .subscribe((response) => this.onFetchedPokemonData(response));
-  } */
+    this.pokeData.fetchPokemonData(url)
+    .subscribe(response => this.onFetchedPokemonData(response));
+  }
 }
