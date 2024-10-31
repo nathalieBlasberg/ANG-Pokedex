d1a8940ce465a23418318ee61e8efddebda9ce7e
Bettina Blasberg <bettina.blasberg@yahoo.de>
2024-11-01 00:28:29 +0100

    refactoring

--- src/app/pokemon-entry/pokemon-entry.component.ts
+++ src/app/pokemon-entry/pokemon-entry.component.ts
@@ -1,32 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PokeDataService } from '../poke-data.service';
-import { Pokemon, PokemonSpecies } from '../pokedex.model';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { PokeDataService } from "../../services/poke-data.service";
+import { Pokemon } from "../../models/pokedex.model";
 
 @Component({
-  selector: 'app-pokemon-entry',
-  templateUrl: './pokemon-entry.component.html',
-  styleUrls: ['./pokemon-entry.component.scss']
+  selector: "app-pokemon-entry",
+  templateUrl: "./pokemon-entry.component.html",
+  styleUrls: ["./pokemon-entry.component.scss"],
 })
 export class PokemonEntryComponent implements OnInit {
-
   public pokemon$?: Pokemon;
   public pokeName?: string;
   public pokeSprite?: string;
-  public pokeSpecies$?: PokemonSpecies;
+  //public pokeSpecies$?: PokemonSpecies;
   public pokeGenus?: string;
   public pokeDescription?: string;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
-    private service: PokeDataService
-  ) { }
+    private router: Router //private service: PokeDataService
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.pokeName = params.get("name")!;
-      this.service.fetchPokedexEntry(this.pokeName)
+      /* this.service.fetchPokedexEntry(this.pokeName)
         .subscribe(response => {
           this.onFetchedPokedexEntry(response);
         }, error => {
@@ -37,25 +35,23 @@ export class PokemonEntryComponent implements OnInit {
           this.onFetchedPokeSpecies(response);
         }, error => {
           this.onCatchedError(error.status);
-        })
-    }
-    );
+        }) */
+    });
   }
 
   onFetchedPokedexEntry(pokedexEntry: Pokemon): void {
     this.pokemon$ = pokedexEntry;
-    this.pokeSprite = this.pokemon$.sprites.other['official-artwork'].front_default;
+    this.pokeSprite =
+      this.pokemon$.sprites.other["official-artwork"].front_default;
   }
 
-  onFetchedPokeSpecies(pokeSpecies: PokemonSpecies): void {
+  /* onFetchedPokeSpecies(pokeSpecies: PokemonSpecies): void {
     this.pokeSpecies$ = pokeSpecies;
     this.pokeGenus = this.pokeSpecies$.genera.filter(genus => genus.language.name === "en")[0].genus;
     this.pokeDescription = this.pokeSpecies$.flavor_text_entries.filter(text => text.language.name === "en")[0].flavor_text;
-  }
+  } */
 
   onCatchedError(status: number) {
-    if(status === 404)
-    this.router.navigate(['**']);
+    if (status === 404) this.router.navigate(["**"]);
   }
-
 }
